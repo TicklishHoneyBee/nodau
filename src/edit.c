34fb8001a73dbf1dd46733be18e497898f490ca8
darkrose <lisa@ltmnet.com>
2012-05-02 20:07:28 +1000

    add GPL exception for linking against OpenSSL to source files

--- src/edit.c
+++ src/edit.c
@@ -13,8 +13,18 @@
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 * See the GNU General Public License for more details.
 *
-* You should have received a copy of the GNU General Public License along
-* with this program.  If not, see <http://www.gnu.org/licenses/>
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>
+*
+* In addition, as a special exception, the copyright holder (Lisa Milne)
+* gives permission to link the code of this release of nodau with the
+* OpenSSL project's "OpenSSL" library (or with modified versions of it
+* that use the same license as the "OpenSSL" library), and distribute
+* the linked executables. You must obey the GNU General Public License
+* in all respects for all of the code used other than "OpenSSL". If you
+* modify this file, you may extend this exception to your version of the
+* file, but you are not obligated to do so. If you do not wish to do so,
+* delete this exception statement from your version.
 ************************************************************************/
 
 #include <unistd.h>
